5a522a16c2734603d3b7629b9329f94a8d8c50e1
M.Hunain Khalid <muhammad.khalid@10pearls.com>
2020-07-08 02:25:00 +0500

    Understanding websockets and sockets io

--- app.js
+++ app.js
@@ -64,7 +64,11 @@ mongoose
     "mongodb+srv://hunain:O18z7KeN2G9oLP7e@cluster0-95unc.mongodb.net/messages?retryWrites=true&w=majority"
   )
   .then(result => {
-    app.listen(8080);
-    console.log("CONNECTED")
+    const server = app.listen(8080);
+    console.log('Connected');
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
